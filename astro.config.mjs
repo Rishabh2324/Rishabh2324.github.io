80856584b9b3474f7a513b24c8269d35ba59fa5a
Rishabh2324 <rishabh2401jain@gmail.com>
2023-07-30 19:03:33 +0530

    Removed base from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rishabh2324.github.io/',
-    base:'/Rishabh2324.github.io'
+    site: 'https://rishabh2324.github.io/'
 });
