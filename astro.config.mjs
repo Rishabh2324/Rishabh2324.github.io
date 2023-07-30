578a926bf644fa4f5b1cb06d198d319ccb414b10
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Pro.local>
2023-07-30 18:25:13 +0530

    Added new 2023 portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://rishabh2324.github.io/',
+    base:'/Rishabh2324.github.io'
+});
